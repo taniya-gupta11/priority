f89401dcaeaee140f7c30b85f471ba05a4475d07
taniya-gupta11 <taniyaguptatg04@gmail.com>
2025-08-31 12:27:39 -0700

    Remove myenv from repo and add .gitignore

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Load trained model and vectorizer
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.form["message"]
+
+    # Transform message using vectorizer
+    data = vectorizer.transform([message]).toarray()
+
+    # Predict using model
+    prediction = model.predict(data)[0]
+    result = "Spam" if prediction == 1 else "Not Spam"
+
+    return render_template("index.html", prediction_text=f"The message is: {result}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
