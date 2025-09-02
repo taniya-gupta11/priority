54396c0771b98c9fc0002f964e7402e4dc8ab466
taniya-gupta11 <taniyaguptatg04@gmail.com>
2025-09-02 12:30:28 -0700

    firsr commit

--- app.py
+++ app.py
@@ -24,5 +24,8 @@ def predict():
 
     return render_template("index.html", prediction_text=f"The message is: {result}")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
